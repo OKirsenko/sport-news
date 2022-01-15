b0d5ffc6ba0d4eb2752fe77e8fba64bf2854d3cb
OKirsenko <senka3434@gmail.com>
2022-01-15 16:17:37 +0200

    pagination

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import './sass/main.scss';
 
 const news = document.querySelector('.news-wrap');
+const moreBtn = document.querySelector('.more-btn');
 
-const pageCount = 1;
-const baseUrl =
-  'https://newsapi.org/v2/top-headlines?' +
-  'country=ua&' +
-  'category=sports&' +
-  'pageSize=20&' +
-  `page=${pageCount}&` +
-  'apiKey=58507d4f9e44467cac1401fbd4d3c576';
+let pageCount = 1;
+let baseUrl = `https://newsapi.org/v2/top-headlines?country=ua&category=sports&pageSize=6&page=${pageCount}&apiKey=58507d4f9e44467cac1401fbd4d3c576`;
 
 function fetchNews(url) {
   return fetch(url).then(response => {
@@ -26,20 +21,28 @@ function articleMarkup(data) {
   const articles = data.articles;
   const markup = articles
     .map(article => {
-      console.log(article);
+      //   console.log(article);
       return `
     <div class="news-card">
-  <p class="news-author"><a href="${article.url}">${article.author}</a></p>
+  <p class="news-author"><a href="${article.url}" target="_blank">${article.author}</a></p>
   <img src="${article.urlToImage}" alt="${article.author}" />
   <div class="news-text">
     <h2 class="news-title">${article.title}</h2>
     <p class="news-descr">${article.description}
-    <a href="${article.url}" class="news-link">Читати далі</a>
+    <a href="${article.url}" class="news-link" target="_blank">Читати далі</a>
     </p>
   </div>
 </div>`;
     })
     .join('');
-  console.log(markup);
-  news.innerHTML = markup;
+  //   console.log(markup);
+  news.insertAdjacentHTML('beforeend', markup);
 }
+
+function onMoreBtnClick() {
+  pageCount += 1;
+  let baseUrl = `https://newsapi.org/v2/top-headlines?country=ua&category=sports&pageSize=6&page=${pageCount}&apiKey=58507d4f9e44467cac1401fbd4d3c576`;
+  console.log(baseUrl);
+  fetchNews(baseUrl).then(articleMarkup);
+}
+moreBtn.addEventListener('click', onMoreBtnClick);
